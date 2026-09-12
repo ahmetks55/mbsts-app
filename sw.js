212a4110be39fef5e871cb188a576630e4dfd593
ahmetks55 <ahmetks55@users.noreply.github.com>
2026-09-12 15:33:48 +0300

    feat: add 2018 MBSTS questions (60 questions, IDs 591-650)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'mbsts-v13';
+const CACHE_NAME = 'mbsts-v14';
 const ASSETS = [
   '/',
   '/index.html',
@@ -11,6 +11,7 @@ const ASSETS = [
   '/questions-2021.json',
   '/questions-2020.json',
   '/questions-2019.json',
+  '/questions-2018.json',
   '/manifest.json',
   '/icons/icon-192.png',
   '/icons/icon-512.png'
